f97807d0c0bd5c13cbc4cd4f6ed15faaf9879f88
Bolek <103572991+BoloMasta@users.noreply.github.com>
2022-10-05 14:12:29 +0200

    main

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,19 +5,27 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-stopBtn.disabled = true;
+// stopBtn.disabled = true;
 let timerId = null;
 
+// start button
 startBtn.addEventListener('click', () => {
   timerId = setInterval(() => {
     document.body.style.backgroundColor = getRandomHexColor();
   }, 1000);
-  startBtn.disabled = true;
-  stopBtn.disabled = false;
+  //startBtn.disabled = true;
+  //stopBtn.disabled = false;
+
+  startBtn.classList.add('disabled-btn');
+  stopBtn.classList.remove('disabled-btn');
 });
 
+//stop button
 stopBtn.addEventListener('click', () => {
   clearInterval(timerId);
-  stopBtn.disabled = true;
-  startBtn.disabled = false;
+  //stopBtn.disabled = true;
+  //startBtn.disabled = false;
+
+  stopBtn.classList.add('disabled-btn');
+  startBtn.classList.remove('disabled-btn');
 });
